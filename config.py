902c54437ebba4d2dc25ae13514895357f58c9b4
mireille1999 <muhawenimana920@daviscollege.com>
2021-03-30 17:34:14 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+
+class Config:
+    """
+    General configuration parent class
+    """
+    #pass
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER ='smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with General
+        configuration settings
+    """
+    # pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General
+        configuration settings
+    """
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mireille:potatopeel420@localhost/pitches'
+    DEBUG = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig,
+    'test' : TestConfig
+}
---
+++ start.ch
@@ -0,0 +1,6 @@
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mireille:potatopeel420@localhost/pitches
+export MAIL_USERNAME='mmuhawenimana920@daviscollege.com'
+export MAIL_PASSWORD='hahahaha'
+export SECRET_KEY='secret'
+
+python3.6 manage.py server
